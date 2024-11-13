78dd1fc30acc620f00d1398d3028a24c906d6e29
Jens Poulsen <jpoulsen@iworq.com>
2024-11-13 11:34:46 -0700

    add image uploading

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-const supabaseUrl = 'https://dynshrpbybaldunbaqjc.supabase.co';
+export const supabaseUrl = 'https://dynshrpbybaldunbaqjc.supabase.co';
 const supabaseKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR5bnNocnBieWJhbGR1bmJhcWpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE0NDU0OTYsImV4cCI6MjA0NzAyMTQ5Nn0.nmje0TxIwUyCGjULyTfCM0CsnmtNg7LvxeM6Y2QjQE0';
 const supabase = createClient(supabaseUrl, supabaseKey);

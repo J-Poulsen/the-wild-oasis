c8880c250be87ff7b349f647cbc58706888d114d
Jens Poulsen <jpoulsen@iworq.com>
2025-01-02 09:13:59 -0700

    remove default login

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from './useLogin';
 import SpinnerMini from '../../ui/SpinnerMini';
 
 function LoginForm() {
-  const [email, setEmail] = useState('testing@testing.com');
-  const [password, setPassword] = useState('asdfasdf');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
